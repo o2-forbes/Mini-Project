e23ab4c84555391dedbc8e083b5a99080b7fb9ac
o2-forbes <oliver2.forbes@live.uwe.ac.uk>
2025-01-22 23:50:04 +0000

    4

--- scene.js
+++ scene.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { createCamera } from "./camera.js";
 
 export function createScene() {
   // Initial scene setup
@@ -6,19 +7,7 @@ export function createScene() {
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(0x777777);
 
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    gameWindow.offsetWidth / gameWindow.offsetHeight,
-    0.1,
-    1000
-  );
-  let cameraRadius = 4;
-  let cameraAzimuth = 0;
-  let cameraElevation = 0;
-  let isMouseDown = false;
-  let prevMouseX = 0;
-  let prevMouseY = 0;
-  updateCameraPosition();
+  const camera = createCamera(gameWindow);
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(gameWindow.offsetWidth, gameWindow.offsetHeight);
@@ -30,10 +19,7 @@ export function createScene() {
   scene.add(mesh);
 
   function draw() {
-    //mesh.rotation.x += 0.01;
-    //mesh.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
+    renderer.render(scene, camera.camera);
   }
 
   function start() {
@@ -45,42 +31,15 @@ export function createScene() {
   }
 
   function onMouseDown() {
-    console.log("mousedown");
-    isMouseDown = true;
+    camera.onMouseDown();
   }
 
   function onMouseUp() {
-    console.log("mouseup");
-    isMouseDown = false;
+    camera.onMouseUp();
   }
 
   function onMouseMove(event) {
-    console.log("mousemove");
-
-    if (isMouseDown) {
-      cameraAzimuth += -(event.clientX - prevMouseX) * 0.5;
-      cameraElevation += (event.clientY - prevMouseY) * 0.5;
-      cameraElevation = Math.min(90, Math.max(0, cameraElevation));
-      updateCameraPosition();
-    }
-
-    prevMouseX = event.clientX;
-    prevMouseY = event.clientY;
-  }
-
-  function updateCameraPosition() {
-    camera.position.x =
-      cameraRadius *
-      Math.sin((cameraAzimuth * Math.PI) / 180) *
-      Math.cos((cameraElevation * Math.PI) / 180);
-    camera.position.y =
-      cameraRadius * Math.sin((cameraElevation * Math.PI) / 180);
-    camera.position.z =
-      cameraRadius *
-      Math.cos((cameraAzimuth * Math.PI) / 180) *
-      Math.cos((cameraElevation * Math.PI) / 180);
-    camera.lookAt(0, 0, 0);
-    camera.updateMatrix();
+    camera.onMouseMove(event);
   }
 
   return {
