711d942ad3b5af616225a22b30bf2218ce9d8634
o2-forbes <oliver2.forbes@live.uwe.ac.uk>
2025-01-22 23:43:41 +0000

    3

--- scene.js
+++ scene.js
@@ -12,7 +12,13 @@ export function createScene() {
     0.1,
     1000
   );
-  camera.position.z = 5;
+  let cameraRadius = 4;
+  let cameraAzimuth = 0;
+  let cameraElevation = 0;
+  let isMouseDown = false;
+  let prevMouseX = 0;
+  let prevMouseY = 0;
+  updateCameraPosition();
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(gameWindow.offsetWidth, gameWindow.offsetHeight);
@@ -21,9 +27,12 @@ export function createScene() {
   const geometry = new THREE.BoxGeometry(1, 1, 1);
   const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
   const mesh = new THREE.Mesh(geometry, material);
-  scene.add();
+  scene.add(mesh);
 
   function draw() {
+    //mesh.rotation.x += 0.01;
+    //mesh.rotation.y += 0.01;
+
     renderer.render(scene, camera);
   }
 
@@ -35,8 +44,50 @@ export function createScene() {
     renderer.setAnimationLoop(null);
   }
 
+  function onMouseDown() {
+    console.log("mousedown");
+    isMouseDown = true;
+  }
+
+  function onMouseUp() {
+    console.log("mouseup");
+    isMouseDown = false;
+  }
+
+  function onMouseMove(event) {
+    console.log("mousemove");
+
+    if (isMouseDown) {
+      cameraAzimuth += -(event.clientX - prevMouseX) * 0.5;
+      cameraElevation += (event.clientY - prevMouseY) * 0.5;
+      cameraElevation = Math.min(90, Math.max(0, cameraElevation));
+      updateCameraPosition();
+    }
+
+    prevMouseX = event.clientX;
+    prevMouseY = event.clientY;
+  }
+
+  function updateCameraPosition() {
+    camera.position.x =
+      cameraRadius *
+      Math.sin((cameraAzimuth * Math.PI) / 180) *
+      Math.cos((cameraElevation * Math.PI) / 180);
+    camera.position.y =
+      cameraRadius * Math.sin((cameraElevation * Math.PI) / 180);
+    camera.position.z =
+      cameraRadius *
+      Math.cos((cameraAzimuth * Math.PI) / 180) *
+      Math.cos((cameraElevation * Math.PI) / 180);
+    camera.lookAt(0, 0, 0);
+    camera.updateMatrix();
+  }
+
   return {
     start,
     stop,
+    onMouseDown,
+    onMouseUp,
+    onMouseMove,
   };
 }
