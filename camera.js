48259ea98148396b12c6c3eebcf6b613e48a9209
o2-forbes <oliver2.forbes@live.uwe.ac.uk>
2025-01-23 00:13:11 +0000

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 
 export function createCamera(gameWindow) {
+  const DEG2RAD = Math.PI / 180.0; // Convert degrees to radians
   const LEFT_MOUSE_BUTTON = 0;
   const MIDDLE_MOUSE_BUTTON = 1;
   const RIGHT_MOUSE_BUTTON = 2;
@@ -16,6 +17,7 @@ export function createCamera(gameWindow) {
     0.1,
     1000
   );
+  let cameraOrigin = new THREE.Vector3(0, 1, 0);
   let cameraRadius = 4;
   let cameraAzimuth = 0;
   let cameraElevation = 0;
@@ -29,14 +31,13 @@ export function createCamera(gameWindow) {
   function updateCameraPosition() {
     camera.position.x =
       cameraRadius *
-      Math.sin((cameraAzimuth * Math.PI) / 180) *
-      Math.cos((cameraElevation * Math.PI) / 180);
-    camera.position.y =
-      cameraRadius * Math.sin((cameraElevation * Math.PI) / 180);
+      Math.sin(cameraAzimuth * DEG2RAD) *
+      Math.cos(cameraElevation * DEG2RAD);
+    camera.position.y = cameraRadius * Math.sin(cameraElevation * DEG2RAD);
     camera.position.z =
       cameraRadius *
-      Math.cos((cameraAzimuth * Math.PI) / 180) *
-      Math.cos((cameraElevation * Math.PI) / 180);
+      Math.cos(cameraAzimuth * DEG2RAD) *
+      Math.cos(cameraElevation * DEG2RAD);
     camera.lookAt(0, 0, 0);
   }
 
@@ -97,12 +98,15 @@ export function createCamera(gameWindow) {
     if (isMiddleMouseDown) {
       const forward = new THREE.Vector3(0, 0, 1).applyAxisAngle(
         Y_AXIS,
-        (cameraAzimuth * Math.PI) / 180
+        cameraAzimuth * DEG2RAD
       );
       const left = new THREE.Vector3(1, 0, 0).applyAxisAngle(
         Y_AXIS,
-        (cameraAzimuth * Math.PI) / 180
+        cameraAzimuth * DEG2RAD
       );
+      cameraOrigin.add(forward.multiplyScalar(-0.01 * deltaY));
+      cameraOrigin.add(left.multiplyScalar(-0.01 * deltaX));
+      updateCameraPosition();
     }
 
     prevMouseX = event.clientX;
